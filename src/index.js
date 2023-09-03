537027728e59bf5c65ce5e0b6f4e0cafad6fdee7
Venura Wijenayake <venu.wije@gmail.com>
2023-09-03 02:51:41 -0700

    UI v0.1

--- src/index.js
+++ src/index.js
@@ -5,13 +5,4 @@ import './index.css';
 import App from './pages/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Router><App /></Router>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+root.render(<Router><App /></Router>);
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,29 +1,51 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { getUser } from '../../utilities/users-service';
-import './App.css';
-import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
+import { useState } from "react";
+// import { Routes, Route } from 'react-router-dom';
+import { getUser } from "../../utilities/users-service";
+import "./App.css";
+import AuthPage from "../AuthPage/AuthPage";
+// import NewOrderPage from '../NewOrderPage/NewOrderPage';
+// import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+// import NavBar from "../../components/NavBar/NavBar";
+// import hotBg from "../../assets/hot.jpg";
+import coldBg from "../../assets/cold.jpg";
+import Descriptions from "../../components/Descriptions/Descriptions";
 
 export default function App() {
   const [user, setUser] = useState(getUser());
 
   return (
-    <main className="App">
-      { user ?
-          <>
-            <NavBar user={user} setUser={setUser} />
-            <Routes>
-              {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
-            </Routes>
-          </>
-          :
-          <AuthPage setUser={setUser} />
-      }
+    <main className="App" style={{ backgroundImage: `url(${coldBg})` }}>
+      {user ? (
+        <div className="overlay">
+          {/* <NavBar user={user} setUser={setUser} /> */}
+
+          <div className="container">
+            <div className="section section__inputs">
+              <input type="text" name="city" placeholder="Enter City ..." />
+              <button>°F</button>
+            </div>
+
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>London, GB</h3>
+                <img
+                  src="https://openweathermap.org/img/wn/02d@2x.png"
+                  alt="weatherIcon"
+                />
+                <h3>Cloudy</h3>
+              </div>
+              <div className="temperature">
+                <h1>34 °C</h1>
+              </div>
+            </div>
+
+            {/* .bottom description */}
+            <Descriptions/>
+          </div>
+        </div>
+      ) : (
+        <AuthPage setUser={setUser} />
+      )}
     </main>
   );
 }
