54e5a1287ae2b6fff036fee0568581cc63443690
Venura Wijenayake <venu.wije@gmail.com>
2023-09-04 07:27:45 -0700

    Checkpoint V0.5

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1a58d79a1fmsh664009e8a01300dp10293bjsn5e89f3f6932c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "8503f224e0a9b107e4f752d3a21f3cb2";
